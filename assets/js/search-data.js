fd3c5b3eeb910108cb48ab7150cb8886d74b49ae
cleo-b-neuropsychologue <cleo.b.psy@gmail.com>
2025-09-01 14:39:38 +0000

    Deploying to gh-pages from @ cleo-b-neuropsychologue/cleo-b-neuropsychologue.github.io@7ce5570821bf3e7dde136c85b5e6a12dab0988b0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,13 +3,20 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-accueil",
-    title: "Accueil",
+    id: "nav-domaines-d-expertise",
+    title: "Domaines d'expertise",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-mon-parcours",
+  },{id: "nav-domaines-d-39-expertise",
+          title: "Domaines d&#39;expertise",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/";
+          },
+        },{id: "nav-mon-parcours",
           title: "Mon Parcours",
           description: "",
           section: "Navigation",
